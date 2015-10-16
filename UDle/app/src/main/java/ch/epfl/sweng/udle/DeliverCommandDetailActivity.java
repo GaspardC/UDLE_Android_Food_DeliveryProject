cc747ba8162391c0a1b239159bcf20368dd60b2c
Gaspard Chevassus <chevassusgaspard@gmail.com>
2015-10-16 17:58:35 +0200

    first commit with all activities 2

---
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/DeliverCommandDetailActivity.java
@@ -0,0 +1,13 @@
+package ch.epfl.sweng.udle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeliverCommandDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deliver_command_detail);
+    }
+}
