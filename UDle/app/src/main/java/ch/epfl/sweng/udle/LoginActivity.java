8cfab89512ba3be9df014f93ff63c692f2ab4970
Gaspard Chevassus <chevassusgaspard@gmail.com>
2015-10-17 15:28:05 +0200

    first facebook connection

--- UDle/app/src/main/java/ch/epfl/sweng/udle/LoginActivity.java
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/LoginActivity.java
@@ -1,13 +1,103 @@
 package ch.epfl.sweng.udle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.TextView;
 
-public class LogInActivity extends AppCompatActivity {
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class LogInActivity extends Activity {
+
+    private LoginButton loginButton;
+    private TextView info;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_log_in);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        info = (TextView)findViewById(R.id.info);
+
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setReadPermissions("public_profile");
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                /*info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );*/
+                Profile profile = Profile.getCurrentProfile();
+                Log.d("Success", "" +profile);
+
+                // Get User Name
+                info.setText(profile.getName() + "");
+
+                GraphRequest request = GraphRequest.newMeRequest( AccessToken.getCurrentAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object,GraphResponse response) {
+                                try {
+                                    String  email=object.getString("email");
+                                    Log.d( "user email ", "email");
+                                } catch (JSONException e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                        });
+
+                request.executeAsync();
+
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
--- UDle/app/src/main/java/ch/epfl/sweng/udle/StartActivity.java
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/StartActivity.java
@@ -1,13 +1,25 @@
 package ch.epfl.sweng.udle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start2);
+        setContentView(R.layout.activity_start);
     }
+    /** Called when the user clicks the LogInButton button */
+    public void goToLoginActivity(View view) {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
 }
