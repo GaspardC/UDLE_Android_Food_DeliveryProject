acf65f38c483a553c16c78c5a3d0212cb10d8191
RA-Danny <rodrigues.alves.danny@gmail.com>
2015-10-19 17:15:03 +0200

    MenuActivity Skelton done.

--- UDle/app/src/main/java/ch/epfl/sweng/udle/MenuActivity.java
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/MenuActivity.java
@@ -1,7 +1,9 @@
 package ch.epfl.sweng.udle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    /** Called when the user clicks the MapActivity button */
+    public void goToOptionsActivity(View view) {
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
     }
 }
