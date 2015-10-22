e775b1849c531d38febb6980e3c3d0e6cb2dd14b
Gaspard Chevassus <chevassusgaspard@gmail.com>
2015-10-23 00:25:01 +0200

    LoginActivity cleaner and goToMapActivity automatically after connected successfully to your Facebook account

--- UDle/app/src/main/java/ch/epfl/sweng/udle/LoginActivity.java
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/LoginActivity.java
@@ -1,17 +1,13 @@
 package ch.epfl.sweng.udle;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
@@ -22,7 +18,6 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
@@ -37,15 +32,19 @@ public class LoginActivity extends Activity {
     private TextView info;
     private static ProfilePictureView profilePictureView;
     private CallbackManager callbackManager;
-    private String user = "";
+    private String mail = "";
     private static String id = "";
     private static String name= "";
     private ProfileTracker mProfileTracker;
+    private Context context = null;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getApplication();
+
         FacebookSdk.sdkInitialize(getApplicationContext());
 
 
@@ -71,8 +70,10 @@ public class LoginActivity extends Activity {
         Profile profile = Profile.getCurrentProfile();
 
         if (profile != null) {
+            setUserInformation(profile);
             info.setText("Hi again " + profile.getFirstName() + "");
-            profilePictureView.setProfileId(profile.getId());
+            goToMapActivityIn(2000);
+
         }
 
 
@@ -87,66 +88,10 @@ public class LoginActivity extends Activity {
             @Override
             public void onSuccess(final LoginResult loginResult) {
 
+                retrieveFacebookInfoFirstConnection(loginResult);
+                retrieveFacebookInfoSecondAndAfterConnections();
 
-                /* Use for the frist connection */
-                mProfileTracker = new ProfileTracker() {
-                    @Override
-                    protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
-
-                       if(profile2!=null){
-                           Log.v("facebook - profile", profile2.getFirstName());
-                           mProfileTracker.stopTracking();
-
-                           AccessToken accessToken = loginResult.getAccessToken();
-
-                           // Get User Name
-                           info.setText("Hello "+profile2.getFirstName() + "");
-                           id = profile2.getId();
-                           name = profile2.getName();
-                           profilePictureView.setProfileId(id);
-                       }
-
-                    }
-                };
-                mProfileTracker.startTracking();
-
-
-                /*Use for 2nd and next connections*/
-                Profile profile = Profile.getCurrentProfile();
-                if(profile == null){
-                    return ;
-                }
-                Log.d("Success", "" + profile);
-
-                // Get User Name
-                info.setText("Hello "+profile.getFirstName() + "");
-                id = profile.getId();
-                name = profile.getName();
-                profilePictureView.setProfileId(id);
-
-
-                GraphRequest request = GraphRequest.newMeRequest(
-                        AccessToken.getCurrentAccessToken(),
-                        new GraphRequest.GraphJSONObjectCallback() {
-                            @Override
-                            public void onCompleted(
-                                    JSONObject object,
-                                    GraphResponse response) {
-                                // Application code
-                                Log.d("response", "response" + object.toString());
-                                try {
-                                    object.getJSONObject("picture").getJSONObject("data").getString("url");
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender,birthday,picture.width(300)");
-                request.setParameters(parameters);
-                request.executeAsync();
-
-
+                goToMapActivityIn(2000);
             }
 
             @Override
@@ -162,22 +107,92 @@ public class LoginActivity extends Activity {
         });
     }
 
+    private void goToMapActivityIn(int i) {
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Go to Map Activity after i ms
+                Intent intent = new Intent(context, MapActivity.class);
+                startActivity(intent);
+
+            }
+        }, i);
+    }
+
+    private void retrieveFacebookInfoSecondAndAfterConnections() {
+         /*Use for 2nd and next connections*/
+        Profile profile = Profile.getCurrentProfile();
+        if(profile == null){
+            return ;
+        }
+        Log.d("Success", "" + profile);
+        setUserInformation(profile);
+
+    }
+
+    private void setUserInformation(Profile profile) {
+        // Get User Name..
+        info.setText("Hello " + profile.getFirstName() + "");
+        id = profile.getId();
+        name = profile.getName();
+        profilePictureView.setProfileId(id);
+        setUserMail();
+    }
+
+    private void setUserMail() {
+
+        //             Use it to get the mail
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        // Application code
+                        Log.d("response", "response" + object.toString());
+                        try {
+                            object.getJSONObject("picture").getJSONObject("data").getString("url");
+                            mail = object.getString("email");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,gender,birthday,picture.width(300)");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    private void retrieveFacebookInfoFirstConnection(final LoginResult loginResult) {
+         /* Use for the first connection */
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+
+                if(profile2!=null){
+                    mProfileTracker.stopTracking();
+                    setUserInformation(profile2);
+                }
+            }
+        };
+        mProfileTracker.startTracking();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    /** Called when the user clicks the MapActivity button */
-    public void goToMapActivity(View view) {
-        Intent intent = new Intent(this, MapActivity.class);
-        startActivity(intent);
 
-
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mProfileTracker.stopTracking();
     }
-
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        profileTracker.stopTracking();
-//    }
 }
