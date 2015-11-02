fa52bc7b461458fd474c8f45c6c974d83a9f1110
Gaspard Chevassus <chevassusgaspard@gmail.com>
2015-11-02 17:06:50 +0100

    get UserLocation and Restaurants availabe around him

--- UDle/app/src/main/java/ch/epfl/sweng/udle/network/DataManager.java
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/network/DataManager.java
@@ -1,17 +1,30 @@
 package ch.epfl.sweng.udle.network;
 
 import android.location.Location;
+import android.util.Log;
 
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.epfl.sweng.udle.Food.OrderElement;
 
 
 /**
  * Created by rodri on 23/10/2015.
  */
+
 public class DataManager {
+    public static final double MAX_DISTANCE_IN_KM_TO_FIND_A_RESTAURANT = 10;
+    public ParseGeoPoint userLocation;
+    public   ArrayList<ParseUser> listAvailableRestaurant;
 
     public ParseUser getCurrentParseUser() {
 
@@ -28,16 +41,50 @@ public class DataManager {
         user.put("Location", point);
         user.saveInBackground();
 
-    }
+        getUserLocation();
 
+    }
+    public void getUserLocation(){
+        ParseUser user = getCurrentParseUser();
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.getInBackground(user.getObjectId(), new GetCallback<ParseUser>() {
+            public void done(ParseUser object, ParseException e) {
+                userLocation  = object.getParseGeoPoint("Location");
+                // This will throw an exception, since the ParseUser is not authenticated
+                object.saveInBackground();
+                getRestaurantLocations(userLocation);
+            }
+        });
+
+    }
 
 
     //info all restaurant around the user location
     //get all restaurant locations, return all restaurant in a perimeter of 5km
-    public void getRestaurantLocations(Location currLocation){
-        //TODO
-    }
+    public void getRestaurantLocations(ParseGeoPoint currLocation) {
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("RestaurantOwner", "YES");
+        query.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null) {
+                    // The query was successful.
+                    Log.d("obj", objects.toString());
+                    listAvailableRestaurant= new ArrayList<ParseUser>();
 
+                    for(ParseUser restoUser : objects){
+                        ParseGeoPoint locResto = restoUser.getParseGeoPoint("Location");
+                        double km = locResto.distanceInKilometersTo(userLocation);
+                        if(km<MAX_DISTANCE_IN_KM_TO_FIND_A_RESTAURANT){
+                        listAvailableRestaurant.add(restoUser);
+                        }
+
+                    }
+                } else {
+                    // Something went wrong.
+                }
+            }
+        });
+    }
     public void getOrder(int userId, int restaurantId){
         //TODO
     }
