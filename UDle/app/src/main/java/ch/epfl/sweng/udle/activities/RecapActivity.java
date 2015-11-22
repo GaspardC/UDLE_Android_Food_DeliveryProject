3c98ddc02f8e7a5c70471cc47465dd5bc7f9a2b2
Hilali Abdessamad <abdessamad.hilali@epfl.ch>
2015-11-22 19:21:33 +0100

    Delete element in recap

--- UDle/app/src/main/java/ch/epfl/sweng/udle/activities/RecapActivity.java
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/activities/RecapActivity.java
@@ -1,9 +1,13 @@
 package ch.epfl.sweng.udle.activities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -24,10 +28,11 @@ import ch.epfl.sweng.udle.R;
 import ch.epfl.sweng.udle.network.DataManager;
 
 public class RecapActivity extends AppCompatActivity {
-
+    AlertDialog.Builder dlgAlert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dlgAlert = new AlertDialog.Builder(this);
         setContentView(R.layout.activity_recap);
 
         OrderElement order = Orders.getActiveOrder();
@@ -57,6 +62,32 @@ public class RecapActivity extends AppCompatActivity {
                                                 new int[] {R.id.RecapElem, R.id.RecapPriceElem, R.id.RecapOptionsString});
         ListView listView = (ListView) findViewById(R.id.RecapActivity_recapListView);
         listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int pos, long id) {
+                Log.v("long clicked", "pos: " + pos);
+                dlgAlert.setMessage("Voulez cous supprimer cette element?");
+                dlgAlert.setTitle(R.string.app_name);
+                dlgAlert.setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                dlgAlert.create().dismiss();
+                            }
+                        });
+                dlgAlert.setNegativeButton("No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                dlgAlert.create().dismiss();
+                            }
+                        });
+                dlgAlert.setCancelable(true);
+                dlgAlert.create().show();
+                return true;
+            }
+        });
     }
 
 
