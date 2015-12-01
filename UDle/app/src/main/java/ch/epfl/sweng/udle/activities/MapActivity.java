f983be62d2dceefddbaa772d16d95cafe72c8b79
Rodrigues Alves Danny <rodrigues.alves.danny@gmail.com>
2015-12-01 13:53:57 +0100

    Update MapActivity.java

--- UDle/app/src/main/java/ch/epfl/sweng/udle/activities/MapActivity.java
+++ UDle/app/src/main/java/ch/epfl/sweng/udle/activities/MapActivity.java
@@ -66,9 +66,6 @@ public class MapActivity extends SlideMenuActivity implements AdapterView.OnItem
         setContentView(R.layout.activity_map);
 
         listeMarkers = new ArrayList<Marker>();
-
-        listeMarkers = new ArrayList<Marker>();
-
         markerLayout = (LinearLayout) findViewById(R.id.locationMarker);
         markerHidden = false;
         dlgAlert = new AlertDialog.Builder(this);
