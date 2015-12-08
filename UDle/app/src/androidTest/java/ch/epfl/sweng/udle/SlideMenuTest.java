de42eb48e247643f8164201ef925e7db39240674
menkid <johan_cattin@hotmail.com>
2015-12-08 21:08:47 +0100

    helpPage: remove work which is not related to the help pages

--- UDle/app/src/androidTest/java/ch/epfl/sweng/udle/SlideMenuTest.java
+++ UDle/app/src/androidTest/java/ch/epfl/sweng/udle/SlideMenuTest.java
@@ -18,36 +18,4 @@ import static android.support.test.espresso.matcher.ViewMatchers.*;
  * Created by Johan on 07.12.2015.
  */
 public class SlideMenuTest {
-
-    public ActivityTestRule<LightActivity> mActivityRule = new ActivityTestRule<>(LightActivity.class, false);
-    private LightActivity activity;
-
-    @Before
-    public void setUp() throws Exception {
-        activity = mActivityRule.launchActivity(new Intent());
-    }
-
-    @Test
-    public void visibility(){
-        onView(withId(R.id.help_page_imageView)).check(matches(isCompletelyDisplayed()));
-        onView(withId(R.id.slideMenu_frame)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
-    }
-
-    @Test
-    public void swipeOpeningClosing(){
-        onView(withId(R.id.help_page_imageView)).perform(swipeRight());
-        onView(withId(R.id.slideMenu_frame)).check(matches(isDisplayed()));
-        onView(withId(R.id.help_page_imageView)).perform(swipeLeft());
-        onView(withId(R.id.help_page_imageView)).check(matches(isCompletelyDisplayed()));
-        onView(withId(R.id.slideMenu_frame)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
-    }
-}
-
-class LightActivity extends SlideMenuActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.help_page);
-    }
 }
